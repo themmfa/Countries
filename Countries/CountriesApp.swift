0e5cce8d0ed6d9d1af7386d85fb30362bb236897
ferdogan <fatih.erdogan@innology.com.tr>
2022-10-07 10:23:40 +0300

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Fatih Erdoğan on 7.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
